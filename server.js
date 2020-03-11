5d8566bd9d02df0fd405a647a451a8043e2a2d13
Jacob Shaeffer <jshaeffer@azmag.gov>
2020-03-11 09:15:18 -0700

    use Date builtin function for string conversion

--- server.js
+++ server.js
@@ -74,14 +74,8 @@ app.post("/contacts", function(req, res) {
 });
 		
 getDate = function(timestamp){
-	let date = new Date(timestamp * 1000);
-	let hours = date.getHours();
-	let minutes = "0" + date.getMinutes();
-	let seconds = "0" + date.getSeconds();
-
-	let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-
-	return formattedTime;
+	let date = (new Date(timestamp * 1000)).toLocaleString();
+	return date;
 }
 
 /*  "/contacts/:id"
