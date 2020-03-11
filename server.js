7a0e6cabe2f45cb8a8550d12918f66b97e3096de
Jacob Shaeffer <jshaeffer@azmag.gov>
2020-03-11 08:57:20 -0700

    changed from at render time to at creation time

--- server.js
+++ server.js
@@ -58,6 +58,7 @@ app.get("/contacts", function(req, res) {
 app.post("/contacts", function(req, res) {
   var newContact = req.body;
   newContact.createDate = new Date();
+  newContact.humanReadable = getDate(newContact.timestamp);
 
 //   if (!(req.body.firstName || req.body.lastName)) {
 //     handleError(res, "Invalid user input", "Must provide a first or last name.", 400);
@@ -71,6 +72,17 @@ app.post("/contacts", function(req, res) {
     }
   });
 });
+		
+getDate = function(timestamp){
+	let date = new Date(timestamp * 1000);
+	let hours = date.getHours();
+	let minutes = "0" + date.getMinutes();
+	let seconds = "0" + date.getSeconds();
+
+	let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+	return formattedTime;
+}
 
 /*  "/contacts/:id"
  *    GET: find contact by id
